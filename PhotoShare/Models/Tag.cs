799defced957cdae78c1ddc1a3c468a674efbc37
Emily Dunn <emilydunn13@gmail.com>
2025-01-14 13:09:14 -0400

    Added and edited some files

---
+++ PhotoShare/Models/Tag.cs
@@ -0,0 +1,18 @@
+﻿namespace PhotoShare.Models
+{
+    public class Tag
+    {
+        //Primary key
+        public int TagId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        //Foreign key
+        public int PhotoID { get; set; }
+
+        //Navigation property - we use it to load the related object. A tag belongs to a photo. Should always be nullable
+        public Photo? Photo { get; set; } //nullable
+
+    }   
+}
+
